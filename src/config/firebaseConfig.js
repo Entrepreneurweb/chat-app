753083a449921e25e8f84198cf3498ec0c764952
Entrepreneurweb <105437348+Entrepreneurweb@users.noreply.github.com>
2024-01-19 11:11:39 +0200

    Update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -6,13 +6,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAII8YcUu_s7AjgY7Z66sIAvyw5O8Ul5pQ",
-  authDomain: "serverfirebaseexo.firebaseapp.com",
-  databaseURL: "https://serverfirebaseexo-default-rtdb.firebaseio.com",
-  projectId: "serverfirebaseexo",
-  storageBucket: "serverfirebaseexo.appspot.com",
-  messagingSenderId: "183502347702",
-  appId: "1:183502347702:web:d3f8b3e67736fad46ab517"
+    apiKey: XXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXX,
+  databaseURL: XXXXXXXXXXXXXXXXXXXX,
+  projectId: XXXXXXXXXXXXXXXXXXX,
+  storageBucket: XXXXXXXXXXXXXXXXXXXX,
+  messagingSenderId: XXXXXXXXXXXXXXX,
+  appId: XXXXXXXXXXXXXXXXXXXXX
 };
 
 // Initialize Firebase
